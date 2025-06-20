d007663a5e3d501bcd0188131aedacc435c45093
ChillyPigeon742 <alekdokmajyan1@gmail.com>
2025-06-20 13:38:14 +0400

    Version 1.0.1
    Changed location of valve.webm to garrysmod/media
    
    Updated setup.exe
    
    Added 2012 valve intro
    
    Updated lua to have better menu detection sorta GMod doesnt have a nice way to detect when the menu.html loads so now i use a manual command with a timer
    
    Added valve 2012 intro video as an extra in the repo

--- ValveIntro/valve_intro.lua
+++ ValveIntro/valve_intro.lua
@@ -1,23 +1,48 @@
-local function PlayValveIntro()
-    RunConsoleCommand("cl_software_cursor", 1)
+local introPlayed = false
+local panel
 
-    local panel = vgui.Create("DHTML")
+local function CleanupPanel(fadeTime)
+    if not IsValid(panel) then return end
+    fadeTime = fadeTime or 1
+
+    if fadeTime > 0 then
+        panel:AlphaTo(0, fadeTime, 0, function()
+            if IsValid(panel) then
+                panel:Remove()
+                RunConsoleCommand("cl_software_cursor", "0")
+            end
+        end)
+    else
+        panel:Remove()
+        RunConsoleCommand("cl_software_cursor", "0")
+    end
+
+    hook.Remove("Think", "ValveIntro_SkipKey")
+end
+
+local function PlayValveIntro()
+    if introPlayed then return end
+    introPlayed = true
+
+    RunConsoleCommand("cl_software_cursor", "1")
+
+    panel = vgui.Create("DHTML")
     panel:SetSize(ScrW(), ScrH())
     panel:SetPos(0, 0)
     panel:SetZPos(32767)
-    panel:SetKeyboardInputEnabled(false)
     panel:SetMouseInputEnabled(false)
+    panel:SetKeyboardInputEnabled(false)
     panel:MakePopup()
 
     panel:SetHTML([[
         <html>
-            <body style="margin:0; overflow:hidden; background:black;">
-                <video id="valveVid" autoplay width="100%" height="100%" style="display:block;">
-                    <source src="asset://garrysmod/html/valve.webm" type="video/webm">
+            <body style="margin:0;overflow:hidden;background:black">
+                <video id="valveVid" autoplay playsinline width="100%" height="100%" style="display:block;object-fit:cover">
+                    <source src="asset://garrysmod/media/valve.webm" type="video/webm">
                 </video>
                 <script>
                     const vid = document.getElementById("valveVid");
-                    vid.onended = function() {
+                    vid.onended = function () {
                         if (typeof gmod !== "undefined" && gmod.VALVE_INTRO_DONE) {
                             gmod.VALVE_INTRO_DONE();
                         }
@@ -28,23 +53,20 @@ local function PlayValveIntro()
     ]])
 
     panel:AddFunction("gmod", "VALVE_INTRO_DONE", function()
-        if IsValid(panel) then
-            panel:AlphaTo(0, 1, 0, function()
-                if IsValid(panel) then
-                    panel:Remove()
-                    RunConsoleCommand("cl_software_cursor", 0)
-                end
-            end)
+        CleanupPanel(1)
+    end)
+
+    hook.Add("Think", "ValveIntro_SkipKey", function()
+        if not IsValid(panel) then
+            hook.Remove("Think", "ValveIntro_SkipKey")
+            return
+        end
+        if input.IsKeyDown(KEY_ESCAPE) then
+            CleanupPanel(0)
         end
     end)
 end
 
-local playedIntro = false
-
-hook.Add("Think", "ValveIntro_MainMenuCheck", function()
-    if gui.IsGameUIVisible() and not playedIntro then
-        PlayValveIntro()
-        playedIntro = true
-        hook.Remove("Think", "ValveIntro_MainMenuCheck")
-    end
+concommand.Add("play_valve_intro", function()
+    timer.Simple(1, PlayValveIntro)
 end)
